c605728cd299c0a7f0974a0f83bc043a811a9688
ibrohim74 <xibroxim11@gmail.com>
2022-12-05 21:18:47 -0600

    asdasD

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 
 var dNoneM = document.querySelectorAll('.d-none-m');
-
+var mobile = document.querySelector('.navMobile')
 function myFunction(x) {
     dNoneM.forEach((item)=>{
         if (x.matches) { // If media query matches
             item.style.display = "none";
+            mobile.style.display= 'inline-block'
         } else {
             item.style.display = "flex";
+            mobile.style.display= 'none'
         }
     })
-
 }
 
-var x = window.matchMedia("(max-width: 700px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+var x = window.matchMedia("(max-width: 992px)")
+myFunction(x)
+x.addListener(myFunction)
